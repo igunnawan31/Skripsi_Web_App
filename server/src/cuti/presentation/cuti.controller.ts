05d7879dbff96444c70d3136734cb2873f291633
alcendol <gibranaldisar@gmail.com>
2025-12-08 03:07:00 +0700

    fix: cancel authorization

--- server/src/cuti/presentation/cuti.controller.ts
+++ server/src/cuti/presentation/cuti.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { SubmitCutiUseCase } from '../application/use-cases/submit-cuti.use-case';
 import { ICutiRepository } from '../domain/repositories/cuti.repository.interface';
 import { CreateCutiDTO } from '../application/dtos/request/create-cuti.dto';
@@ -9,9 +20,10 @@ import { CutiFilterDTO } from '../application/dtos/request/filter-cuti.dto';
 import { UserRequest } from 'src/common/types/UserRequest.dto';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { AuthGuard } from '@nestjs/passport';
-import { MinorRole } from '@prisma/client';
+import { MajorRole, MinorRole } from '@prisma/client';
 import { RolesMinor } from 'src/common/decorators/minor-role.decorator';
 import { ApprovalCutiDTO } from '../application/dtos/request/approval.dto';
+import { RolesMajor } from 'src/common/decorators/major-roles.decorator';
 
 @Controller('cuti')
 @UseGuards(AuthGuard('jwt'), RolesGuard)
@@ -22,31 +34,43 @@ export class CutiController {
     private readonly ApproveCutiUseCase: ApproveCutiUseCase,
     private readonly CancelCutiUseCase: CancelCutiUseCase,
     private readonly RejectCutiUseCase: RejectCutiUseCase,
-  ) { }
+  ) {}
 
   // POST cuti/
   @Post()
-  create(@Body() data: CreateCutiDTO, @Req() req: Request & { user: UserRequest }) {
+  create(
+    @Body() data: CreateCutiDTO,
+    @Req() req: Request & { user: UserRequest },
+  ) {
     return this.submitCutiUseCase.execute(req.user.id, data);
   }
 
   // GET cuti/
   @Get()
-  findAll(@Req() req: Request & { user: UserRequest }, @Query() filters: CutiFilterDTO) {
+  findAll(
+    @Req() req: Request & { user: UserRequest },
+    @Query() filters: CutiFilterDTO,
+  ) {
     return this.cutiRepo.findAll(req.user, filters);
   }
 
   // GET cuti/team
   @Get('team')
   @RolesMinor(MinorRole.PROJECT_MANAGER)
-  findTeamCuti(@Req() req: Request & { user: UserRequest }, @Query() filters: CutiFilterDTO) {
+  findTeamCuti(
+    @Req() req: Request & { user: UserRequest },
+    @Query() filters: CutiFilterDTO,
+  ) {
     return this.cutiRepo.findTeamCuti(req.user, filters);
   }
 
   // GET cuti/pending
   @Get('pending')
   @RolesMinor(MinorRole.HR)
-  findAllPendingForApprover(@Req() req: Request & { user: UserRequest }, @Query() filters: CutiFilterDTO) {
+  findAllPendingForApprover(
+    @Req() req: Request & { user: UserRequest },
+    @Query() filters: CutiFilterDTO,
+  ) {
     return this.cutiRepo.findPendingForApprover(req.user, filters);
   }
 
@@ -64,22 +88,34 @@ export class CutiController {
   // PATCH cuti/approve/:id
   @Patch('approve/:id')
   @RolesMinor(MinorRole.HR)
-  approval(@Param('id') id: string, @Body() data: ApprovalCutiDTO, @Req() req: Request & { user: UserRequest }) {
-    return this.ApproveCutiUseCase.execute(id, req.user.id, data)
+  approval(
+    @Param('id') id: string,
+    @Body() data: ApprovalCutiDTO,
+    @Req() req: Request & { user: UserRequest },
+  ) {
+    return this.ApproveCutiUseCase.execute(id, req.user.id, data);
   }
 
   // PATCH cuti/reject/:id
   @Patch('reject/:id')
   @RolesMinor(MinorRole.HR)
-  reject(@Param('id') id: string, @Body() data: ApprovalCutiDTO, @Req() req: Request & { user: UserRequest }) {
-    return this.RejectCutiUseCase.execute(id, req.user.id, data)
+  reject(
+    @Param('id') id: string,
+    @Body() data: ApprovalCutiDTO,
+    @Req() req: Request & { user: UserRequest },
+  ) {
+    return this.RejectCutiUseCase.execute(id, req.user.id, data);
   }
 
   // PATCH cuti/cancel/:id
   @Patch('cancel/:id')
-  @RolesMinor(MinorRole.HR)
-  cancel(@Param('id') id: string, @Body() data: ApprovalCutiDTO, @Req() req: Request & { user: UserRequest }) {
-    return this.CancelCutiUseCase.execute(id, req.user.id, data)
+  @RolesMajor(MajorRole.KARYAWAN)
+  cancel(
+    @Param('id') id: string,
+    @Body() data: ApprovalCutiDTO,
+    @Req() req: Request & { user: UserRequest },
+  ) {
+    return this.CancelCutiUseCase.execute(id, req.user.id, data);
   }
 
   // DELETE cuti/:id
