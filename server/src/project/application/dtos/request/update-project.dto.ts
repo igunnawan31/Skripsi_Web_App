a4ffe1cc1c3565c4a8dfc7c3df0bde077eb1d9b4
alcendol <gibranaldisar@gmail.com>
2025-12-27 16:01:15 +0700

    fix: file handling on create, update, delete

--- server/src/project/application/dtos/request/update-project.dto.ts
+++ server/src/project/application/dtos/request/update-project.dto.ts
@@ -1,10 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDTO } from './create-project.dto';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsArray, IsDate, IsEnum, IsOptional, IsString } from 'class-validator';
 import { ProjectStatus } from '@prisma/client';
+import { FileMetaData } from 'src/common/types/FileMetaData.dto';
+import { Type } from 'class-transformer';
 
 export class UpdateProjectDTO extends PartialType(CreateProjectDTO) {
   @IsEnum(ProjectStatus)
   @IsOptional()
   status?: ProjectStatus;
+
+  @IsArray()
+  @IsOptional()
+  removeDokumen?: string[];
+}
+export class InternalUpdateProjectDTO extends OmitType(UpdateProjectDTO, [
+  'startDate',
+  'endDate',
+] as const) {
+  @Type(() => Date)
+  @IsDate()
+  @IsOptional()
+  startDate?: Date;
+
+  @Type(() => Date)
+  @IsOptional()
+  @IsDate()
+  endDate?: Date;
+
+  @IsOptional()
+  dokumen?: FileMetaData[];
 }
