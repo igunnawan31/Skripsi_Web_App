b239f659aa18d0d7cd6a3d8d37276bf673886c7c
alcendol <gibranaldisar@gmail.com>
2026-02-21 14:17:33 +0700

    feat: reimburse and salary listener done tested

--- server/src/modules/notifications/notifications.module.ts
+++ server/src/modules/notifications/notifications.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
+import { NotificationsController } from './presentation/notifications.controller';
+import { CreateNotificationUseCase } from './application/use-cases/create-notifications.use-cases';
+import { DeleteNotificationsUseCase } from './application/use-cases/delete-notifications.use-case';
+import { GetAllNotificationsUseCase } from './application/use-cases/get-all-notifications.use-case';
+import { GetNotificationUseCase } from './application/use-cases/get-notification.use-case';
+import { INotificationRepository } from './domain/repositories/notifications.repository.interface';
+import { NotificationRepository } from './infrastructure/persistence/notifications.repository';
+import { ReimburseApprovedListener } from './infrastructure/listeners/reimburse-updated.listener';
+import { SalaryPaidListener } from './infrastructure/listeners/salary-paid.listener';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [
+    CreateNotificationUseCase,
+    DeleteNotificationsUseCase,
+    GetAllNotificationsUseCase,
+    GetNotificationUseCase,
+    ReimburseApprovedListener,
+    SalaryPaidListener,
+    { provide: INotificationRepository, useClass: NotificationRepository },
+  ],
 })
 export class NotificationsModule {}
